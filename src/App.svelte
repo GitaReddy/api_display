bb342c44eb2b786b2714d8c10f3932c8dd30ee2b
geethasree.puli <geethasree.puli@gmail.com>
2023-05-29 19:01:53 +0530

    data display from api done

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,48 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	import { onMount } from "svelte";
+	import Grid from "gridjs-svelte";
+	// import './app.css';
+  
+	let jsonData = [];
+	let tableVisible = false;
+  
+	onMount(async () => {
+	  const response = await fetch("https://api.recruitly.io/api/masterdata/industries?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154");
+	  const responseData = await response.json();
+	  jsonData = responseData.data;
+	});
+  
+	function openTable() {
+	  if (!tableVisible) {
+		fetchData();
+	  }
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+  
+	async function fetchData() {
+	  const response = await fetch("https://api.recruitly.io/api/masterdata/industries?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154");
+	  const responseData = await response.json();
+	  jsonData = responseData.data;
+	  tableVisible = true;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  </script>
+  
+  <!-- <button on:click={openTable}>Display Table</button> -->
+  <h1>Data Display from Api</h1>
+  <main>
+	{#if openTable}
+	  <Grid
+		
+		
+		pagination={{ enabled: true, limit: 38 }}
+		data={jsonData.map(item => ({
+			reference: item.id,
+		  title: item.name,
+		  
+		  
+		}))} />
+	{/if}
+  </main>
+  
+  <style global>
+	@import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
+  </style>
